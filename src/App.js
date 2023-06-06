6f3d4eb3697fbc4ee7f4a558980329e86b8111e7
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-06-06 16:40:39 -0400

    state is changing for btn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// Separate named imports, this makes the code more readable
+import { Component } from "react";
+import shortid from "shortid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleOnClick = (e) => {
+    const currentEl = e.currentTarget.name;
+    this.setState((el) => {
+      return { [currentEl]: el[currentEl] + 1 };
+    });
+    console.log(this.state);
+  };
+
+  options = ["good", "neutral", "bad"];
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        {this.options.map((el) => {
+          const short_id = shortid.generate();
+          return (
+            <div key={short_id}>
+              <button type="button" name={el} onClick={this.handleOnClick}>
+                {el}
+              </button>
+            </div>
+          );
+        })}
+        <ul>
+          <li>
+            <p>Good:{good}</p>
+          </li>
+          <li>
+            <p>Neutral:{neutral}</p>
+          </li>
+          <li>
+            <p>Bad:{bad}</p>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
-
 export default App;
