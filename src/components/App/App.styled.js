1c2ec00f44e8c18fb9e2c734570c7525012e15ca
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-06-07 02:32:54 -0400

    media query added

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -14,6 +14,9 @@ export const Container = styled.div`
   box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
     rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
   transition: transform 0.4s;
+  @media (max-width: 520px) {
+    width: 275px;
+  }
   &:hover {
     transition: transform 0.4s;
     transform: scale(1.05);
